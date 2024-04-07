1916bb8163f3b457c70597933f860c26a5543492
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-04-07 17:47:17 +0530

    Add files via upload

---
+++ codeserver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install code-server
+curl -fsSL https://code-server.dev/install.sh | sh
+
+# Install Nano
+sudo apt install nano
+
+# Start Cloudflare tunnel
+cloudflared tunnel --url localhost:6070 &
+
+# Wait for tunnel to establish (optional, adjust sleep duration as needed)
+sleep 5
+
+# Start code-server in the background
+code-server --port 6070 &
+
+# Wait for 4 seconds
+sleep 4
+
+# Open code-server configuration file for editing with Nano
+nano /root/.config/code-server/config.yaml
