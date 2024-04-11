28e91fce44d6c52fe191244d42a487f18f88c6cd
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-04-11 05:06:53 -0700

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# install nano text editor
-sudo apt install nano
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -0 cloudflared
+ 
+chmod +x cloudflared
+ 
+sudo mv cloudflared /usr/local/bin/
 
-# Install code-server
 curl -fsSL https://code-server.dev/install.sh | sh
 
-# Install Nano
 sudo apt install nano
 
-# Start Cloudflare tunnel
 cloudflared tunnel --url localhost:6070 &
 
-# Wait for tunnel to establish (optional, adjust sleep duration as needed)
 sleep 5
 
-# Start code-server in the background
 code-server --port 6070 &
 
-# Wait for 4 seconds
 sleep 4
 
-# Open code-server configuration file for editing with Nano
 nano /root/.config/code-server/config.yaml
