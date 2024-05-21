0fbccf02281262b98e398fad96fb841cdc50a84d
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-05-21 11:37:31 -0400

    Create code2.sh

---
+++ code2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install localtunnel
+npm install -g localtunnel
+
+# Install code-server
+curl -fsSL https://code-server.dev/install.sh | sh
+
+# Install nano (optional)
+apt update
+apt install nano -y
+
+# Start local Tunnel
+lt --port 6070 &
+
+# Wait for local Tunnel to start
+sleep 5
+
+# Start code-server
+code-server --port 6070 &
+
+# Wait for code-server to start
+sleep 4
+
+# Display code-server configuration
+cat ~/.config/code-server/config.yaml
