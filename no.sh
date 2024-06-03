8e1748a673ef3da7f0e3b87ca1017e0923764075
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-06-03 05:40:56 -0700

    Update no.sh

--- no.sh
+++ no.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+sudo apt update
+sudo apt install nodejs npm -y
+# Download and install localtunnel
+npm install -g localtunnel
 
 # Install code-server
 curl -fsSL https://code-server.dev/install.sh | sh
 
+# Install nano (optional)
+sudo apt update
+sudo apt install nano
+
+# Start local Tunnel
+lt --port 6070 & wget_output=$(wget -q -O - https://loca.lt/mytunnelpassword)
+
+# Extract password and public URL
+tunnel_password=$(echo "$wget_output" | grep -oP '(?<=Tunnel established at ).*(?= (https))')
+public_url=$(echo "$wget_output" | grep -oP 'https://.*')
+
+# Display tunnel password and public URL
+echo "LocalTunnel Password: $tunnel_password"
+echo "Public URL: $public_url"
+
+# Start code-server
 code-server --port 6070 --auth none
 
 # Wait for code-server to start
@@ -10,6 +30,3 @@ sleep 4
 
 # Display code-server configuration
 cat ~/.config/code-server/config.yaml
-git clone https://github.com/epic-miner/option.git
-cd option
-sh nglt.sh
